72c8eec0899b77a436e45be94c758fef2e4f5f85
fendypierre <fendypierre@fendypierres-Mac.local>
2018-05-26 13:08:00 -0700

    retrieved lost files

---
+++ Monch/MainTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarController.swift
+//  Monch
+//
+//  Created by fendypierre on 5/26/18.
+//  Copyright © 2018 ConchNCode. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if Auth.auth().currentUser == nil {
+            //show if not logged in
+            DispatchQueue.main.async {
+                let loginController = LoginController()
+                let navController = UINavigationController(rootViewController: loginController)
+                self.present(navController, animated: true, completion: nil)
+            }
+            
+            return
+        }
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        
+        let navController = UINavigationController(rootViewController: userProfileController)
+        
+        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController, UIViewController()]
+    }
+}
---
+++ Pods/Target Support Files/Pods-Monch/Pods-Monch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Monch : NSObject
+@end
+@implementation PodsDummy_Pods_Monch
+@end
